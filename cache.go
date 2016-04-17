9854fa0bee6a490f615e8b40ab6c6c031ca755be
orivil <307509265@qq.com>
2016-04-17 22:26:00 +0800

    add data cacher

---
+++ cache.go
@@ -0,0 +1,227 @@
+package cache
+
+import (
+	"sync"
+)
+
+type tire struct {
+	data interface{}
+	prev *tire
+	next *tire
+}
+
+type Cache struct {
+	intIndex map[int]*tire
+	sync.RWMutex
+	maxID    int
+}
+
+func New() *Cache {
+	return &Cache{
+		intIndex:make(map[int]*tire, 1000),
+	}
+}
+
+func (this *Cache) Del(id int) {
+	this.Lock()
+	defer this.Unlock()
+	if t, ok := this.intIndex[id]; ok {
+		if t.prev != nil {
+			t.prev.next = t.next
+		}
+		if t.next != nil {
+			t.next.prev = t.prev
+		}
+		delete(this.intIndex, id)
+	}
+}
+
+func (this *Cache) Update(id int, ins interface{}) {
+	this.RLock()
+	t, ok := this.intIndex[id]
+	this.RUnlock()
+	if ok {
+		// ignore data race
+		t.data = ins
+	} else {
+		this.Add(id, ins)
+	}
+}
+
+// if has id not exist in cache, it will call 'dataProvider', and cache the result
+func (this *Cache) GetIn(ids []int, dataProvider func(id int)interface{}) (datas []interface{}) {
+	this.RLock()
+	datas = make([]interface{}, len(ids))
+	for i, id := range ids {
+		if t, ok := this.intIndex[id]; ok {
+			datas[i] = t.data
+		} else {
+			this.RUnlock()
+			// data not exist in cache, get it form 'dataProvider'
+			data := dataProvider(id)
+			// add to cache
+			datas[i] = data
+			this.Add(id, data)
+			this.RLock()
+		}
+	}
+	this.RUnlock()
+	return
+}
+
+func (this *Cache) GetNext(startID, num int, orderAsc bool) (datas []interface{}) {
+	datas = make([]interface{}, num)
+
+	this.RLock()
+	defer this.RUnlock()
+	maxID := this.maxID
+	if startID > maxID {
+		return nil
+	} else if startID < 1 {
+		startID = 1
+	}
+
+	for startID <= maxID {
+		if t, ok := this.intIndex[startID]; ok {
+			// asc
+			if orderAsc {
+				index := 0
+				for {
+					if num > index {
+						datas[index] = t.data
+						if t.next == nil {
+							return datas[0:index + 1]
+						}
+						t = t.next
+						index++
+					} else {
+						return
+					}
+				}
+			// desc
+			} else {
+				index := num - 1
+				for {
+					if index >= 0 {
+						datas[index] = t.data
+						if t.next == nil {
+							return datas[index:]
+						}
+						t = t.next
+						index--
+					} else {
+						return
+					}
+				}
+			}
+		} else {
+			startID++
+		}
+	}
+
+	return nil
+}
+
+func (this *Cache) GetPrev(startID, num int, orderDesc bool) (datas []interface{}) {
+	datas = make([]interface{}, num)
+
+	this.RLock()
+	defer this.RUnlock()
+	if startID > this.maxID {
+		startID = this.maxID
+	} else if startID < 1 {
+		// get the last data
+		startID = this.maxID
+	}
+	for startID > 0 {
+		if t, ok := this.intIndex[startID]; ok {
+			// asc
+			if !orderDesc {
+				index := 0
+				for {
+					if num > index {
+						datas[index] = t.data
+						if t.prev == nil {
+							return datas[0:index + 1]
+						}
+						t = t.prev
+						index++
+					} else {
+						return
+					}
+				}
+			// desc
+			} else {
+				index := num - 1
+				for {
+					if index >= 0 {
+						datas[index] = t.data
+						if t.prev == nil {
+							return datas[index:]
+						}
+						t = t.prev
+						index--
+					} else {
+						return
+					}
+				}
+			}
+		} else {
+			startID--
+		}
+	}
+
+	return nil
+}
+
+func (this *Cache) Add(id int, ins interface{}) {
+	var prevID int
+	var nextID int
+	var toTop bool
+	var toButtom bool
+
+	this.Lock()
+	defer this.Unlock()
+	if _, ok := this.intIndex[id]; !ok {
+		if this.maxID < id {
+			this.maxID = id
+		}
+		t := &tire{data: ins}
+		this.intIndex[id] = t
+		index := 1
+		for !toTop || !toButtom {
+			prevID = id - index
+			nextID = id + index
+			// add the tire to prev
+			if prevID > 0 {
+				if prevT, ok := this.intIndex[prevID]; ok {
+					if prevT.next != nil {
+						prevT.next.prev = t
+						t.next = prevT.next
+					}
+					prevT.next = t
+					t.prev = prevT
+					break
+				}
+			} else {
+				toTop = true
+			}
+			// add the tire to next
+			if nextID <= this.maxID {
+				if nextT, ok := this.intIndex[nextID]; ok {
+					if nextT.prev != nil {
+						nextT.prev.next = t
+						t.prev = nextT.prev
+					}
+					nextT.prev = t
+					t.next = nextT
+					break
+				}
+			} else {
+				toButtom = true
+			}
+			// add both sides
+			index++
+		}
+	}
+}
--- example_test.go
+++ example_test.go
@@ -2,9 +2,6 @@ package cache_test
 import (
 	"log"
 	"fmt"
-	"os"
-	"path/filepath"
-	"time"
 	"gopkg.in/orivil/cache.v0"
 )
 
@@ -14,8 +11,28 @@ type data struct {
 	Name string
 }
 
-func ExampleJsonCache() {
-	// data with default value
+func ExampleRead() {
+	d := &data{Name: "golang"}
+
+	// 1. new JsonCache
+	cache, err := cache.NewJsonCache(dir, "data.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// 2. read exist or not exist file
+	// if dir not exist, it will be auto generated
+	err = cache.Read(d)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(d.Name == "foobar")
+
+	// Output:
+	// true
+}
+
+func ExampleWrite() {
 	d := &data{Name: "foobar"}
 
 	// 1. new JsonCache
@@ -24,32 +41,10 @@ func ExampleJsonCache() {
 		log.Fatal(err)
 	}
 
-	// 2. read not exist file
-	// if dir not exist, it will be auto generated
-	err = cache.Read(d)
-	if err != nil {
-		log.Fatal(err)
-	}
-	// use default value
-	fmt.Println(d.Name == "foobar")
-
-	// 3. cache data to file
-	// it will generate a new file
+	// 2. cache data to file
+	// it will cover the exist file or generate a new file
 	err = cache.Write(d)
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	// 4. read the file
-	d = &data{}
-	cache.Read(d)
-	fmt.Println(d.Name == "foobar")
-
-	// this test will generate a file in 'testdata' dir, and it will be removed after 10 second
-	time.Sleep(10 * time.Second)
-	os.Remove(filepath.Join(dir, "data.json"))
-
-	// Output:
-	// true
-	// true
 }
